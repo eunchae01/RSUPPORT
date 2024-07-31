d2b837bcbc73d9cf2ebb122518414f0086b24bdd
Eun Chae Cho <eunchae01@naver.com>
2024-07-31 16:49:01 +0900

    [ init ] first commit

---
+++ noitce/src/main/java/com/rsupport/noitce/NoitceApplication.java
@@ -0,0 +1,13 @@
+package com.rsupport.noitce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoitceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoitceApplication.class, args);
+	}
+
+}
